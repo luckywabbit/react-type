a892fde9b1e6d91ddf73983ce98bb3d754240774
TMT <michael@luckywabbit.com>
2023-08-04 14:14:50 -0400

    Jade Emperor edit

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
